9ba1dfc400504cd4010101bad62fb080d1d65ece
unknown <wb-dsy346555@alibaba-inc.com>
2018-05-14 17:44:10 +0800

    'add-depressedBorder.html'

--- ua.js
+++ ua.js
@@ -27,8 +27,11 @@ _a.fn.init.prototype = _a.fn;
 // ua中的扩展方法 可以用于扩展ua的接口，也可以拷贝对象
 _a.extend = _a.fn.extend = function() { // boolean, obj1, obj2 ...
   var target = arguments[0] || {}, // 拷贝的目标
-      i = 1, // 需要遍历的次数
+      i = 1, // 需要拷贝的源对象从第几个参数开始，默认从第二个开始拷贝
       length, // 参数个数
+      options, // 当前被遍历到的对象
+      src, // 
+      copy,
       deep = false; // 深拷贝
   
   //判断是否为深拷贝
@@ -38,15 +41,28 @@ _a.extend = _a.fn.extend = function() { // boolean, obj1, obj2 ...
   }
 
   // 如果target不是object
-  if(typeof target !== 'object') {
+  if(typeof target !== 'object' && typeof target !== 'function') {
     target = {};
   }
 
   // 如果只有一个参数
   if(length === i) {
     target = this;
-    --i;
+    --i; // 如果只有一个参数则目标为this 而所有的参数都是源对象
   }
-  
+
+  for(; i < length; i++){
+    if(options = arguments[i] != null) {
+      for(name in options) {
+        src = target[name];
+        copy = options[name];
+      }
+    }
+  }
+
   return target;
 }
+
+/**
+ * sizzle 选择器引擎
+ */
