906f88fe86c9ada2311753a391dea8acb4a4ca80
unknown <wb-dsy346555@alibaba-inc.com>
2018-06-01 17:33:27 +0800

    'add_test'

---
+++ javascript books/javascriptInHtml.js
@@ -0,0 +1,10 @@
+
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('DOM loaded');
+});
+
+window.addEventListener('load', function() {
+    console.log('load');
+});
+
+console.log('zero', document.querySelector('#box'));
